6ffb1e76a1a4e76c0eaa533057942606b1d839c3
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2026-04-05 22:12:35 +0200

    added image card in HomeScreen

--- app/src/main/java/it/unibo/trace/ui/screens/HomeScreen.kt
+++ app/src/main/java/it/unibo/trace/ui/screens/HomeScreen.kt
@@ -1,9 +1,14 @@
 package it.unibo.trace.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -14,8 +19,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import it.unibo.trace.ui.composables.Footer
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import it.unibo.trace.R
 import it.unibo.trace.ui.composables.Header
+import it.unibo.trace.ui.composables.ImageCard
 
 @Composable
 fun HomeScreen(
@@ -46,11 +54,21 @@ fun HomeScreen(
             }
         }
     ) { innerPadding ->
-        // Content goes here
-        Box(
-            modifier = Modifier.padding(innerPadding)
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .padding(horizontal = 16.dp)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            // Screen content
+            repeat(5) { index ->
+                ImageCard(
+                    title = "Card Random #${index + 1}",
+                    description = "Example of description",
+                    imagePainter = painterResource(id = R.drawable.ic_launcher_foreground)
+                )
+            }
         }
     }
 }

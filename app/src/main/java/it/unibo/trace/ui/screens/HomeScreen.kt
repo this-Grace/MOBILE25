9edd4a358e869a241751e3f3860acce4ba919bf3
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2026-04-05 21:34:49 +0200

    changed shape of floatingActionButton in a circle

--- app/src/main/java/it/unibo/trace/ui/screens/HomeScreen.kt
+++ app/src/main/java/it/unibo/trace/ui/screens/HomeScreen.kt
@@ -2,6 +2,8 @@ package it.unibo.trace.ui.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
@@ -10,6 +12,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import it.unibo.trace.ui.composables.Header
 
 @Composable
@@ -23,6 +27,9 @@ fun HomeScreen(
         },
         floatingActionButton = {
             FloatingActionButton(
+                modifier = Modifier
+                    .size(56.dp)
+                    .clip(CircleShape),
                 onClick = onAddClick,
                 containerColor = MaterialTheme.colorScheme.primary
             ) {

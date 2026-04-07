a05e822e692054dc7d9863cdf26abcd1e4bf5ad0
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2026-04-07 09:46:44 +0200

    created FloatingButton

---
+++ app/src/main/java/it/unibo/trace/ui/composables/FloatingButton.kt
@@ -0,0 +1,51 @@
+package it.unibo.trace.ui.composables
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun FloatingButton(
+    onClick: () -> Unit = {},
+    imageVector: ImageVector,
+    contentDescription: String
+) {
+    FloatingActionButton(
+        onClick = onClick,
+        containerColor = Color.Transparent,
+        contentColor = Color.Black
+    ) {
+        Box(
+            modifier = Modifier
+                .size(56.dp)
+                .background(brush = Brush.linearGradient(
+                        colors = listOf(
+                            Color(0xFF81ECFF),
+                            MaterialTheme.colorScheme.primary
+                        ),
+                        start = Offset(0f, 0f),
+                        end = Offset.Infinite
+                    ),
+                    shape = CircleShape
+                ),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(
+                imageVector = imageVector,
+                contentDescription = contentDescription,
+            )
+        }
+    }
+}
--- app/src/main/java/it/unibo/trace/ui/screens/HomeScreen.kt
+++ app/src/main/java/it/unibo/trace/ui/screens/HomeScreen.kt
@@ -1,33 +1,27 @@
 package it.unibo.trace.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import it.unibo.trace.ui.composables.Footer
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import it.unibo.trace.R
+import it.unibo.trace.ui.composables.FloatingButton
 import it.unibo.trace.ui.composables.Header
 import it.unibo.trace.ui.composables.ImageCard
 
 @Composable
 fun HomeScreen(
-    onAddClick: () -> Unit = {},
+    onFloatingClick: () -> Unit = {},
     onSettingsClick: () -> Unit = {}
 ) {
     val title = "TRACE"
@@ -40,18 +34,10 @@ fun HomeScreen(
             Footer(title)
         },
         floatingActionButton = {
-            FloatingActionButton(
-                modifier = Modifier
-                    .size(56.dp)
-                    .clip(CircleShape),
-                onClick = onAddClick,
-                containerColor = MaterialTheme.colorScheme.primary
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Add,
-                    contentDescription = "Add"
-                )
-            }
+            FloatingButton(
+                onClick = onFloatingClick,
+                imageVector = Icons.Default.Add,
+                contentDescription = "Add")
         }
     ) { innerPadding ->
         Column(

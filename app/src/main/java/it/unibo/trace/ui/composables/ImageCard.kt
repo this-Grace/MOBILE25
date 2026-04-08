f50c0cd40fcb3f04fbb43fbaa881ef311f2a1680
Grazia Bochdanovits <grazia.bochdanovits@studio.unibo.it>
2026-04-08 18:36:33 +0200

    rm unused PhotoCard

--- app/src/main/java/it/unibo/trace/ui/composables/ImageCard.kt
+++ app/src/main/java/it/unibo/trace/ui/composables/ImageCard.kt
@@ -5,23 +5,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
-
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -36,32 +31,22 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun ImageCard(
     imagePainter: Painter,
+    modifier: Modifier = Modifier,
     title: String? = null,
     subtitle: String? = null,
     authorName: String? = null,
     isLiked: Boolean = false,
     showLikeIcon: Boolean = true,
     showOverlay: Boolean = true,
-    aspectRatio: Float? = null,
     onClick: () -> Unit = {},
     onLikeClick: () -> Unit = {}
 ) {
     Card(
         onClick = onClick,
         shape = RoundedCornerShape(12.dp),
-        modifier = Modifier.fillMaxWidth()
+        modifier = modifier
     ) {
-        Box(
-            modifier = if (aspectRatio != null) {
-                Modifier
-                    .fillMaxWidth()
-                    .aspectRatio(aspectRatio)
-            } else {
-                Modifier
-                    .fillMaxWidth()
-                    .height(200.dp)
-            }
-        ) {
+        Box(modifier = Modifier.fillMaxSize()) {
             Image(
                 painter = imagePainter,
                 contentDescription = subtitle ?: title,

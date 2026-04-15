c8c4e108a0c0d74a808c7fd3e140f264e4c7a14e
Grazia Bochdanovits <grazia.bochdanovits@studio.unibo.it>
2026-04-15 12:32:16 +0200

    feat: integrate osmdroid library and permissions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
+    implementation(libs.osmdroid)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)

a587e32a2478b5dc4ac399efd5265763e0c9a1d8
Grazia Bochdanovits <grazia.bochdanovits@studio.unibo.it>
2026-04-17 17:07:28 +0200

    deps: add compose runtime and layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.osmdroid)
+    implementation(libs.androidx.compose.runtime)
+    implementation(libs.androidx.compose.foundation.layout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
